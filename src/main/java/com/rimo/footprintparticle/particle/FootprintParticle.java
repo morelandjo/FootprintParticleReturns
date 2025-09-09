264b97fcfb7e38723aa687d3beddedf1c54d56bf
morelandjo <josh@brightwebdevs.com>
2025-09-09 15:42:37 -0400

    Fixed particle directionality in 3rd person, particles direction is locked instead of following the camera

--- src/main/java/com/rimo/footprintparticle/particle/FootprintParticle.java
+++ src/main/java/com/rimo/footprintparticle/particle/FootprintParticle.java
@@ -8,6 +8,10 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.util.Mth;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.Camera;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 
 import java.util.List;
 
@@ -56,6 +60,42 @@ public class FootprintParticle extends TextureSheetParticle {
 
 		if (this.age++ >= this.lifetime || this.level.isEmptyBlock(net.minecraft.core.BlockPos.containing(Mth.floor(this.x), Mth.floor(this.y - 0.02f), Mth.floor(this.z))))
 			this.remove();
+	}
+
+	@Override
+	public void render(VertexConsumer vertexConsumer, Camera camera, float partialTicks) {
+		// Use ground-oriented rendering instead of billboard
+		Vector3f[] corners = new Vector3f[]{
+			new Vector3f(-1.0F, 0.0F, -1.0F),
+			new Vector3f(-1.0F, 0.0F, 1.0F),
+			new Vector3f(1.0F, 0.0F, 1.0F),
+			new Vector3f(1.0F, 0.0F, -1.0F)
+		};
+
+		float size = this.getQuadSize(partialTicks);
+		
+		// Create rotation around Y-axis for ground orientation
+		Quaternionf rotation = new Quaternionf().rotationY(this.roll);
+
+		// Transform corners and render
+		for (int i = 0; i < 4; ++i) {
+			Vector3f corner = corners[i];
+			corner.mul(size);
+			corner.rotate(rotation);
+			corner.add((float)(this.x - camera.getPosition().x), (float)(this.y - camera.getPosition().y), (float)(this.z - camera.getPosition().z));
+		}
+
+		float u0 = this.getU0();
+		float u1 = this.getU1();
+		float v0 = this.getV0();
+		float v1 = this.getV1();
+		int light = this.getLightColor(partialTicks);
+
+		// Add vertices (ground-oriented quad)
+		vertexConsumer.addVertex(corners[0].x(), corners[0].y(), corners[0].z()).setUv(u1, v1).setColor(this.rCol, this.gCol, this.bCol, this.alpha).setLight(light);
+		vertexConsumer.addVertex(corners[1].x(), corners[1].y(), corners[1].z()).setUv(u1, v0).setColor(this.rCol, this.gCol, this.bCol, this.alpha).setLight(light);
+		vertexConsumer.addVertex(corners[2].x(), corners[2].y(), corners[2].z()).setUv(u0, v0).setColor(this.rCol, this.gCol, this.bCol, this.alpha).setLight(light);
+		vertexConsumer.addVertex(corners[3].x(), corners[3].y(), corners[3].z()).setUv(u0, v1).setColor(this.rCol, this.gCol, this.bCol, this.alpha).setLight(light);
 	}
 
 	public static class DefaultFactory implements ParticleProvider<SimpleParticleType> {
